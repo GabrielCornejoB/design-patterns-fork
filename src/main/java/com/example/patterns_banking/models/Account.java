721bc766780bae3e860d01eb0f4360e148e61731
Gabriel Sebastián Cornejo Botero <gcornejo@bancolombia.com.co>
2025-04-22 09:54:04 -0500

    feat: add account model using builder pattern

---
+++ src/main/java/com/example/patterns_banking/models/Account.java
@@ -0,0 +1,78 @@
+package com.example.patterns_banking.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)private
+    String number;
+
+    @Enumerated(EnumType.STRING)private AccountType type;
+    private BigDecimal balance;
+
+    private Boolean isActive = true;
+
+    public enum AccountType{AHORROS, CORRIENTE};
+
+    private Account(Builder builder) {
+        this.id = builder.id;
+        this.number = builder.number;
+        this.balance = builder.balance;
+        this.isActive = builder.isActive;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+
+    public static class Builder {
+        private Long id;
+        private String number;
+        private BigDecimal balance;
+        private Boolean isActive;
+
+        private Builder() {}
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder number(String number) {
+            this.number = number;
+            return this;
+        }
+
+        public Builder balance(BigDecimal balance) {
+            this.balance = balance;
+            return this;
+        }
+
+        public Builder isActive(Boolean isActive) {
+            this.isActive = isActive;
+            return this;
+        }
+
+        public Account build() {
+            return new Account(this);
+        }
+    }
+}
